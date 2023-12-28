0e9fced4f4fea3f940f80a1c01c2e01c1e1e2b6f
Ms-1020 <1256845994@qq.com>
2023-12-28 16:08:17 +0800

    add Timer

--- src/Timer.cpp
+++ src/Timer.cpp
@@ -2,4 +2,45 @@
 // Created by rjj on 12/27/23.
 //
 
+
+
 #include "include/Timer.h"
+void Timer::AdjustTimer(int id, int new_expires) {
+  assert(!heap_.empty() && ref_.count(new_expires));
+  heap_[ref_[id]].expires = Clock::now() + MS(new_expires);
+  SiftDown(ref_[id], heap_.size());                   //从上往下调整一次
+}
+
+void Timer::AddTimer(int id, int timeout, const std::function<void()> &cb) {
+
+}
+
+void Timer::DoWork(int id) {}
+
+void Timer::Clear() {
+  ref_.clear();
+  heap_.clear();
+}
+
+void Timer::Tick() {}
+
+void Timer::PopTimer() {
+  assert(!heap_.empty());
+  DelTimer(0);            //将堆顶的Timer出去
+}
+
+int Timer::GetNextTick() { return 0; }
+
+void Timer::DelTimer(size_t index) {}
+
+void Timer::SiftUp(size_t index) {}
+
+bool Timer::SiftDown(size_t index, size_t n) { return false; }
+
+void Timer::SwapNode(size_t index1, size_t index2) {
+  assert(index1 >=0 && index1 <= heap_.size());
+  assert(index2 >=0 && index2 <= heap_.size());
+  std::swap(heap_[index1],heap_[index2]);
+  ref_[heap_[index1].id] = index1;
+  ref_[heap_[index2].id] = index2;
+}
--- src/include/Timer.h
+++ src/include/Timer.h
@@ -2,9 +2,59 @@
 // Created by rjj on 12/27/23.
 //
 
-#ifndef DAY01_TIMER_H
-#define DAY01_TIMER_H
+#pragma once
 
-class Timer {};
+#include <queue>
+#include <cerrno>
+#include <cassert>
+#include <functional>
+#include <cstdlib>
+#include <cstring>
+#include <ctime>
+#include <chrono>
+#include <unordered_map>
+#include <sys/socket.h>
+#include <signal.h>
 
-#endif  // DAY01_TIMER_H
+typedef std::chrono::high_resolution_clock Clock;
+typedef Clock::time_point TimeStamp;
+typedef std::chrono::milliseconds MS;
+
+struct TimerNode{
+  int id;
+  TimeStamp expires;                      //超时时间
+  std::function<void()> time_callback;    //任务回调函数
+  bool operator<(const TimerNode& t) {
+    return expires < t.expires;
+  }
+};
+class Timer {
+public:
+  Timer(){heap_.reserve(64);}
+  ~Timer(){Clear();}
+  void AdjustTimer(int id, int new_expires);
+
+  void AddTimer(int id, int timeout, const std::function<void()> &cb);
+
+  void DoWork(int id);
+
+  void Clear();
+
+  void Tick();
+
+  void PopTimer();
+
+  int GetNextTick();
+private:
+  void DelTimer(size_t index);
+
+  void SiftUp(size_t index);
+
+  bool SiftDown(size_t index, size_t n);
+
+  void SwapNode(size_t index1, size_t index2);
+
+  std::vector<TimerNode> heap_;
+  std::unordered_map<int, size_t> ref_;     //建立id到下标的映射
+};
+
